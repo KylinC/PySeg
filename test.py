753f2b60b1b92648ed75d8e5062615ce46e4a16c
KylinC <k1017856853@icloud.com>
2019-12-21 21:47:39 +0800

    official version

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import pyseg
-from tkinter import *
 
 # # Hidden Markov Model
 
@@ -8,4 +7,6 @@ res2 = pyseg.cut('中国特色社会主义，邓小平改革开放')
 res3 = pyseg.cut('发言人强调，该法案涉台内容严重违反一个中国原则和中美三个联合公报规定，严重损害中美关系和台海和平稳定。')
 res4 = pyseg.cut('通知强调，各地要做好统筹安排，按照国务院关于保障义务教育教师工资待遇的工作部署，加大工作力度。')
 
-print(res1)
+for item in res4:
+    print(item,end="")
+    print('\\',end="")
